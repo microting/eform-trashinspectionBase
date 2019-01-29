3fd7964ccfc07e77c6fc2f967296ff5fe5de923a
René Schultz Madsen <rm@microting.dk>
2019-01-29 10:11:34 +0100

    Adding the new migrations.

---
+++ Microting.eFormTrashInspectionBase/Migrations/20190129090817_InitialCreate.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.eFormTrashInspectionBase.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Setup for SQL Server Provider
+
+            var autoIDGenStrategy = "SqlServer:ValueGenerationStrategy";
+            object autoIDGenStrategyValue = SqlServerValueGenerationStrategy.IdentityColumn;
+
+            // Setup for MySQL Provider
+            if (migrationBuilder.ActiveProvider == "Pomelo.EntityFrameworkCore.MySql")
+            {
+                DbConfig.IsMySQL = true;
+                autoIDGenStrategy = "MySql:ValueGenerationStrategy";
+                autoIDGenStrategyValue = MySqlValueGenerationStrategy.IdentityColumn;
+            }
+            migrationBuilder.CreateTable(
+                name: "Installations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Installations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InstallationSiteVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    InstallationId = table.Column<int>(nullable: false),
+                    SDKSiteId = table.Column<int>(nullable: false),
+                    InstallationSiteId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InstallationSiteVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InstallationVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    InstallationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InstallationVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrashInspectionPnSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    SelectedeFormId = table.Column<int>(nullable: true),
+                    SelectedeFormName = table.Column<string>(nullable: true),
+                    Token = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrashInspectionPnSettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrashInspectionPnSettingVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    SelectedeFormId = table.Column<int>(nullable: true),
+                    SelectedeFormName = table.Column<string>(nullable: true),
+                    TrashInspectionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrashInspectionPnSettingVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrashInspections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    WeighingNumber = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    RegistrationNumber = table.Column<string>(nullable: true),
+                    TrashFraction = table.Column<int>(nullable: false),
+                    Eak_Code = table.Column<int>(nullable: false),
+                    Producer = table.Column<string>(nullable: true),
+                    Transporter = table.Column<string>(nullable: true),
+                    InstallationId = table.Column<int>(nullable: false),
+                    MustBeInspected = table.Column<bool>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrashInspections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrashInspectionVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    WeighingNumber = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    RegistrationNumber = table.Column<string>(nullable: true),
+                    TrashFraction = table.Column<int>(nullable: false),
+                    EakCode = table.Column<int>(nullable: false),
+                    Producer = table.Column<string>(nullable: true),
+                    Transporter = table.Column<string>(nullable: true),
+                    InstallationId = table.Column<int>(nullable: false),
+                    MustBeInspected = table.Column<bool>(nullable: false),
+                    TrashInspctionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrashInspectionVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InstallationSites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    InstallationId = table.Column<int>(nullable: false),
+                    SDKSiteId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InstallationSites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InstallationSites_Installations_InstallationId",
+                        column: x => x.InstallationId,
+                        principalTable: "Installations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrashInspectionCases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    TrashInspectionId = table.Column<int>(nullable: false),
+                    SdkCaseId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrashInspectionCases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrashInspectionCases_TrashInspections_TrashInspectionId",
+                        column: x => x.TrashInspectionId,
+                        principalTable: "TrashInspections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InstallationSites_InstallationId",
+                table: "InstallationSites",
+                column: "InstallationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrashInspectionCases_TrashInspectionId",
+                table: "TrashInspectionCases",
+                column: "TrashInspectionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InstallationSites");
+
+            migrationBuilder.DropTable(
+                name: "InstallationSiteVersions");
+
+            migrationBuilder.DropTable(
+                name: "InstallationVersions");
+
+            migrationBuilder.DropTable(
+                name: "TrashInspectionCases");
+
+            migrationBuilder.DropTable(
+                name: "TrashInspectionPnSettings");
+
+            migrationBuilder.DropTable(
+                name: "TrashInspectionPnSettingVersions");
+
+            migrationBuilder.DropTable(
+                name: "TrashInspectionVersions");
+
+            migrationBuilder.DropTable(
+                name: "Installations");
+
+            migrationBuilder.DropTable(
+                name: "TrashInspections");
+        }
+    }
+}
