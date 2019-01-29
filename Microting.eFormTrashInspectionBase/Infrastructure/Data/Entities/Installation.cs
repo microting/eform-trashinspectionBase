bad77fd2742f3b1eb89fe1b33d2e9b8409af7824
René Schultz Madsen <rm@microting.dk>
2019-01-29 07:58:52 +0100

    Adding the relation from Installation to InstallationSite accessible.

--- Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Installation.cs
+++ Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Installation.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
 {
     public class Installation : BaseEntity
     {
-
+        public Installation()
+        {
+            this.InstallationSites = new HashSet<InstallationSite>();
+        }
+        
         public int Version { get; set; }
 
         public string Name { get; set; }
+        
+        public virtual ICollection<InstallationSite> InstallationSites { get; set; }
     }
 }
