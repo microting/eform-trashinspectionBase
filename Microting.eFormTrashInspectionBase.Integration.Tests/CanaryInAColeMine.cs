9b2fbfdc10f9db8b372f3a8ca9fd9dcc9727173b
René Schultz Madsen <rm@microting.dk>
2019-01-29 09:25:29 +0100

    Adding a basic test and fixing travis.

---
+++ Microting.eFormTrashInspectionBase.Integration.Tests/CanaryInAColeMine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Microting.eFormTrashInspectionBase.Integration.Tests
+{
+    [TestFixture]
+    public class CanaryInAColeMine
+    {
+        [Test]
+        public void IntegrationCanPeep()
+        {
+            Assert.True(true);
+        }
+    }
+}
