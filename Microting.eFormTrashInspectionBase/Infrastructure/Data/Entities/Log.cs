e41a78d0baf8febd8b13a8b82f909a711a04b348
René Schultz Madsen <rm@microting.dk>
2019-03-25 14:04:23 +0100

    Adding missing references.

--- Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Log.cs
+++ Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Log.cs
@@ -21,6 +21,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+using Microting.eFormApi.BasePn.Infrastructure.Database.Base;
+
 namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
 {
     public class Log : BaseEntity
