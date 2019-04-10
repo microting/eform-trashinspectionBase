939cef162029e9568a2c08201c43ca36b8447e73
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-04-10 10:20:46 +0200

    Adding Create Update Delete to entity.

--- Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Producer.cs
+++ Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Producer.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Linq;
+using eFormShared;
+using Microting.eFormApi.BasePn.Abstractions;
+using Microting.eFormTrashInspectionBase.Infrastructure.Data.Factories;
+
 namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
 {
-    public class Producer : BaseTrashInspectionEntity
+    public class Producer : BaseTrashInspectionEntity, IDataAccessObject<TrashInspectionPnDbContext>
     {        
         public int Version { get; set; }
 
@@ -19,5 +25,90 @@ namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
         public string Phone { get; set; }
         
         public string ContactPerson { get; set; }
+
+        public void Create(TrashInspectionPnDbContext dbContext)
+        {
+            dbContext.Producers.Add(this);
+            dbContext.SaveChanges();
+
+            dbContext.ProducerVersions.Add(MapVersions(dbContext, this));
+            dbContext.SaveChanges();
+
+            Id = Id;
+        }
+
+        public void Update(TrashInspectionPnDbContext dbContext)
+        {
+            Producer producer = dbContext.Producers.FirstOrDefault(x => x.Id == Id);
+
+            if (producer == null)
+            {
+                throw new NullReferenceException($"Could not find producer with id: {Id}");
+            }
+
+            producer.Name = Name;
+            producer.Description = Description;
+            producer.ForeignId = ForeignId;
+            producer.Address = Address;
+            producer.City = City;
+            producer.ZipCode = ZipCode;
+            producer.Phone = Phone;
+            producer.ContactPerson = ContactPerson;
+
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                producer.UpdatedAt = DateTime.Now;
+                producer.Version += 1;
+                dbContext.SaveChanges();
+
+                dbContext.ProducerVersions.Add(MapVersions(dbContext, producer));
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Delete(TrashInspectionPnDbContext dbContext)
+        {
+            Producer producer = dbContext.Producers.FirstOrDefault(x => x.Id == Id);
+
+            if (producer == null)
+            {
+                throw new NullReferenceException($"Could not find producer with id: {Id}");
+            }
+
+            producer.WorkflowState = Constants.WorkflowStates.Removed;
+
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                                producer.UpdatedAt = DateTime.Now;
+                                producer.Version += 1;
+                                dbContext.SaveChanges();
+                
+                                dbContext.ProducerVersions.Add(MapVersions(dbContext, producer));
+                                dbContext.SaveChanges();
+            }
+        }
+        public ProducerVersion MapVersions(TrashInspectionPnDbContext _dbContext, Producer producer)
+        {
+            ProducerVersion producerVersion = new ProducerVersion();
+
+            producerVersion.Name = producer.Name;
+            producerVersion.Description = producer.Description;
+            producerVersion.ForeignId = producer.ForeignId;
+            producerVersion.Address = producer.Address;
+            producerVersion.City = producer.City;
+            producerVersion.ZipCode = producer.ZipCode;
+            producerVersion.Phone = producer.Phone;
+            producerVersion.ContactPerson = producer.ContactPerson;
+            producerVersion.Version = producer.Version;
+            producerVersion.CreatedAt = producer.CreatedAt;
+            producerVersion.UpdatedAt = producer.UpdatedAt;
+            producerVersion.CreatedByUserId = producer.CreatedByUserId;
+            producerVersion.UpdatedByUserId = producer.UpdatedByUserId;
+            producerVersion.WorkflowState = producer.WorkflowState;
+
+            producerVersion.ProducerId = producer.Id;
+
+            return producerVersion;
+        }
     }
 }
--- Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Transporter.cs
+++ Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/Transporter.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Linq;
+using eFormShared;
+using Microting.eFormApi.BasePn.Abstractions;
+using Microting.eFormTrashInspectionBase.Infrastructure.Data.Factories;
+
 namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
 {
-    public class Transporter : BaseTrashInspectionEntity
+    public class Transporter : BaseTrashInspectionEntity, IDataAccessObject<TrashInspectionPnDbContext>
     {        
         public int Version { get; set; }
 
@@ -19,5 +25,91 @@ namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
         public string Phone { get; set; }
         
         public string ContactPerson { get; set; }
+        
+         public void Create(TrashInspectionPnDbContext dbContext)
+        {
+            dbContext.Transporters.Add(this);
+            dbContext.SaveChanges();
+
+            dbContext.TransporterVersions.Add(MapVersions(dbContext, this));
+            dbContext.SaveChanges();
+
+            Id = Id;
+        }
+
+        public void Update(TrashInspectionPnDbContext dbContext)
+        {
+            Transporter transporter = dbContext.Transporters.FirstOrDefault(x => x.Id == Id);
+
+            if (transporter == null)
+            {
+                throw new NullReferenceException($"Could not find transprter with id: {Id}");
+            }
+
+            transporter.Name = Name;
+            transporter.Description = Description;
+            transporter.ForeignId = ForeignId;
+            transporter.Address = Address;
+            transporter.City = City;
+            transporter.ZipCode = ZipCode;
+            transporter.Phone = Phone;
+            transporter.ContactPerson = ContactPerson;
+
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                transporter.UpdatedAt = DateTime.Now;
+                transporter.Version += 1;
+                dbContext.SaveChanges();
+
+                dbContext.TransporterVersions.Add(MapVersions(dbContext, transporter));
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Delete(TrashInspectionPnDbContext dbContext)
+        {
+            Transporter transporter = dbContext.Transporters.FirstOrDefault(x => x.Id == Id);
+
+            if (transporter == null)
+            {
+                throw new NullReferenceException($"Could not find transporter with id: {Id}");
+            }
+
+            transporter.WorkflowState = Constants.WorkflowStates.Removed;
+
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                transporter.UpdatedAt = DateTime.Now;
+                transporter.Version += 1;
+                dbContext.SaveChanges();
+                
+                dbContext.TransporterVersions.Add(MapVersions(dbContext, transporter));
+                dbContext.SaveChanges();
+            }
+        }
+        
+        public TransporterVersion MapVersions(TrashInspectionPnDbContext _dbContext, Transporter transporter)
+        {
+            TransporterVersion transporterVersion = new TransporterVersion();
+
+            transporterVersion.Name = transporter.Name;
+            transporterVersion.Description = transporter.Description;
+            transporterVersion.ForeignId = transporter.ForeignId;
+            transporterVersion.Address = transporter.Address;
+            transporterVersion.City = transporter.City;
+            transporterVersion.ZipCode = transporter.ZipCode;
+            transporterVersion.Phone = transporter.Phone;
+            transporterVersion.ContactPerson = transporter.ContactPerson;
+            transporterVersion.Version = transporter.Version;
+            transporterVersion.CreatedAt = transporter.CreatedAt;
+            transporterVersion.UpdatedAt = transporter.UpdatedAt;
+            transporterVersion.CreatedByUserId = transporter.CreatedByUserId;
+            transporterVersion.UpdatedByUserId = transporter.UpdatedByUserId;
+            transporterVersion.WorkflowState = transporter.WorkflowState;
+
+            transporterVersion.TransporterId = transporter.Id;
+
+            return transporterVersion;
+        }
     }
 }
