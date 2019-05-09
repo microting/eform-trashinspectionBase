4afe88a2176a2ea9a9185238a428e966cb2247ab
René Schultz Madsen <rm@microting.dk>
2019-05-09 14:28:27 +0200

    Adding more unit tests.

--- Microting.eFormTrashInspectionBase.Unit.Tests/InstallationSiteUTest.cs
+++ Microting.eFormTrashInspectionBase.Unit.Tests/InstallationSiteUTest.cs
@@ -1,7 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities;
+using NUnit.Framework;
+
 namespace Microting.eFormTrashInspectionBase.Unit.Tests
 {
-    public class InstallationSiteUTest
+    [TestFixture]
+    public class InstallationSiteUTest : DbTestFixture
     {
-        
+        [Test]
+        public void InstallationSiteModel_Save_DoesSave()
+        {
+            // Arrange
+            Installation installation = new Installation();
+            installation.CreatedAt = DateTime.Now;
+            installation.Name = Guid.NewGuid().ToString();
+
+            installation.Create(DbContext);
+
+            Random rnd = new Random();
+
+            InstallationSite installationSite = new InstallationSite();
+            installationSite.InstallationId = installation.Id;
+            installationSite.SDKSiteId = rnd.Next(1, 255);
+
+            // Act
+            installationSite.Create(DbContext);
+
+            InstallationSite dbInstallationSite = DbContext.InstallationSites.AsNoTracking().First();
+            List<InstallationSite> installationSiteList = DbContext.InstallationSites.AsNoTracking().ToList();
+            List<InstallationSiteVersion> installationSiteVersions = DbContext.InstallationSiteVersions.AsNoTracking().ToList();
+            // Assert
+            Assert.NotNull(dbInstallationSite);
+
+            Assert.AreEqual(1, installationSiteList.Count());
+            Assert.AreEqual(1, installationSiteVersions.Count());
+
+            Assert.AreEqual(installationSite.CreatedAt.ToString(), dbInstallationSite.CreatedAt.ToString());
+            Assert.AreEqual(installationSite.InstallationId, dbInstallationSite.InstallationId);
+
+
+        }
+
+        [Test]
+        public void InstalationSiteModel_Update_DoesUpdate()
+        {
+            // Arrange
+            Installation installation = new Installation();
+            installation.CreatedAt = DateTime.Now;
+            installation.Name = Guid.NewGuid().ToString();
+
+            installation.Create(DbContext);
+
+            Random rnd = new Random();
+            InstallationSite installationSite = new InstallationSite();
+            installationSite.CreatedAt = DateTime.Now;
+            installationSite.InstallationId = installation.Id;
+            installationSite.SDKSiteId = rnd.Next(1, 255);
+
+            installationSite.Create(DbContext);
+            // Act
+            
+            installationSite.SDKSiteId = rnd.Next(1, 355);
+            installationSite.InstallationId = installation.Id;
+            installationSite.Id = installationSite.Id;
+
+           installationSite.Update(DbContext);
+
+            InstallationSite dbInstallationSite = DbContext.InstallationSites.AsNoTracking().First();
+            List<InstallationSite> installationSiteList = DbContext.InstallationSites.AsNoTracking().ToList();
+            List<InstallationSiteVersion> installationSiteVersions = DbContext.InstallationSiteVersions.AsNoTracking().ToList();
+
+            // Assert
+            Assert.NotNull(dbInstallationSite);
+
+            Assert.AreEqual(1, installationSiteList.Count());
+            Assert.AreEqual(2, installationSiteVersions.Count());
+
+            Assert.AreEqual(installationSite.CreatedAt.ToString(), dbInstallationSite.CreatedAt.ToString());
+            Assert.AreEqual(installationSite.SDKSiteId, dbInstallationSite.SDKSiteId);
+            Assert.AreEqual(installationSite.InstallationId, dbInstallationSite.InstallationId);
+
+        }
+
+        [Test]
+        public void InstallationSiteModel_Delete_DoesDelete()
+        {
+            // Arrange
+            Installation installation = new Installation();
+            installation.CreatedAt = DateTime.Now;
+            installation.Name = Guid.NewGuid().ToString();
+
+            installation.Create(DbContext);
+
+            Random rnd = new Random();
+            InstallationSite installationSite = new InstallationSite();
+            installationSite.CreatedAt = DateTime.Now;
+            installationSite.InstallationId = installation.Id;
+            installationSite.SDKSiteId = rnd.Next(1, 255);
+
+            installationSite.Create(DbContext);
+            // Act            
+
+            installationSite.Delete(DbContext);
+
+            InstallationSite dbInstallationSite = DbContext.InstallationSites.AsNoTracking().First();
+            List<InstallationSite> installationSiteList = DbContext.InstallationSites.AsNoTracking().ToList();
+            List<InstallationSiteVersion> installationSiteVersions = DbContext.InstallationSiteVersions.AsNoTracking().ToList();
+
+            // Assert
+            Assert.NotNull(dbInstallationSite);
+
+            Assert.AreEqual(1, installationSiteList.Count());
+            Assert.AreEqual(2, installationSiteVersions.Count());
+
+            Assert.AreEqual(installationSite.CreatedAt.ToString(), dbInstallationSite.CreatedAt.ToString());
+            Assert.AreEqual(installationSite.SDKSiteId, dbInstallationSite.SDKSiteId);
+            Assert.AreEqual(installationSite.InstallationId, dbInstallationSite.InstallationId);
+            Assert.AreEqual(installationSite.WorkflowState, eFormShared.Constants.WorkflowStates.Removed);
+
+        }
+
     }
 }
--- Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/InstallationSite.cs
+++ Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/InstallationSite.cs
@@ -1,6 +1,8 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using eFormShared;
 
 namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
 {
@@ -12,5 +14,81 @@ namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
         public int InstallationId { get; set; }
 
         public int SDKSiteId { get; set; }
+
+        public void Create(TrashInspectionPnDbContext dbContext)
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            Version = 1;
+            WorkflowState = Constants.WorkflowStates.Created;
+            
+            dbContext.InstallationSites.Add(this);
+            dbContext.SaveChanges();
+
+            dbContext.InstallationSiteVersions.Add(MapVersions(this));
+            dbContext.SaveChanges();
+        }
+
+        public void Update(TrashInspectionPnDbContext dbContext)
+        {
+            InstallationSite installationSite = dbContext.InstallationSites.FirstOrDefault(x => x.Id == Id);
+
+            if (installationSite == null)
+            {
+                throw new NullReferenceException($"Could not find installationSite with id {Id}");
+            }
+
+            installationSite.WorkflowState = WorkflowState;
+            installationSite.InstallationId = InstallationId;
+            installationSite.SDKSiteId = SDKSiteId;
+            
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                installationSite.UpdatedAt = DateTime.UtcNow;
+                installationSite.Version += 1;
+
+                dbContext.InstallationSiteVersions.Add(MapVersions(this));
+                dbContext.SaveChanges();
+            }  
+            
+        }
+
+        public void Delete(TrashInspectionPnDbContext dbContext)
+        {
+            InstallationSite installationSite = dbContext.InstallationSites.FirstOrDefault(x => x.Id == Id);
+
+            if (installationSite == null)
+            {
+                throw new NullReferenceException($"Could not find installationSite with id {Id}");
+            }
+
+            installationSite.WorkflowState = Constants.WorkflowStates.Removed;
+            
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                installationSite.UpdatedAt = DateTime.UtcNow;
+                installationSite.Version += 1;
+
+                dbContext.InstallationSiteVersions.Add(MapVersions(this));
+                dbContext.SaveChanges();
+            }  
+        }
+
+        private InstallationSiteVersion MapVersions(InstallationSite installationSite)
+        {
+            InstallationSiteVersion installationSiteVersion = new InstallationSiteVersion
+            {
+                Version = installationSite.Version,
+                InstallationId = installationSite.InstallationId,
+                SDKSiteId = installationSite.SDKSiteId,
+                CreatedAt = installationSite.CreatedAt,
+                UpdatedAt = installationSite.UpdatedAt,
+                CreatedByUserId = installationSite.CreatedByUserId,
+                UpdatedByUserId = installationSite.UpdatedByUserId,
+                WorkflowState = installationSite.WorkflowState
+            };
+
+            return installationSiteVersion;
+        }
     }
 }
