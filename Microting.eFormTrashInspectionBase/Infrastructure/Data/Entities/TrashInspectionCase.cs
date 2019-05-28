aa30ca93cc823683b6eab22404482ad68ef02907
René Schultz Madsen <rm@microting.dk>
2019-05-28 09:53:30 +0200

    Adding create,update delete methods for trashsinspectioncase.

--- Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/TrashInspectionCase.cs
+++ Microting.eFormTrashInspectionBase/Infrastructure/Data/Entities/TrashInspectionCase.cs
@@ -1,4 +1,7 @@
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using eFormShared;
 
 namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
 {
@@ -17,5 +20,81 @@ namespace Microting.eFormTrashInspectionBase.Infrastructure.Data.Entities
         
         [ForeignKey("Segment")]
         public int SegmentId { get; set; }
+        
+        public void Create(TrashInspectionPnDbContext dbContext)
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            Version = 1;
+            WorkflowState = Constants.WorkflowStates.Created;
+            
+            dbContext.TrashInspectionCases.Add(this);
+            dbContext.SaveChanges();
+
+            dbContext.TrashInspectionCaseVersions.Add(MapVersions(this));
+            dbContext.SaveChanges();
+        }
+
+        public void Update(TrashInspectionPnDbContext dbContext)
+        {
+            TrashInspectionCase trashInspection = dbContext.TrashInspectionCases.FirstOrDefault(x => x.Id == Id);
+
+            if (trashInspection == null)
+            {
+                throw new NullReferenceException($"Could not find TrashInspectionCase with id: {Id}");
+            }
+
+            trashInspection.Status = Status;
+            trashInspection.TrashInspectionId = TrashInspectionId;
+            trashInspection.SdkCaseId = SdkCaseId;
+            trashInspection.SdkSiteId = SdkSiteId;
+            trashInspection.Version = Version;
+            trashInspection.SegmentId = SegmentId;
+
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                trashInspection.UpdatedAt = DateTime.UtcNow;
+                trashInspection.Version += 1;
+
+                dbContext.TrashInspectionCaseVersions.Add(MapVersions(trashInspection));
+                dbContext.SaveChanges();
+            }                       
+        }
+
+        public void Delete(TrashInspectionPnDbContext dbContext)
+        {
+            TrashInspectionCase trashInspection = dbContext.TrashInspectionCases.FirstOrDefault(x => x.Id == Id);
+
+            if (trashInspection == null)
+            {
+                throw new NullReferenceException($"Could not find TrashInspectionCase with id: {Id}");
+            }
+
+            trashInspection.WorkflowState = Constants.WorkflowStates.Removed;
+            
+            if (dbContext.ChangeTracker.HasChanges())
+            {
+                trashInspection.UpdatedAt = DateTime.UtcNow;
+                trashInspection.Version += 1;
+
+                dbContext.TrashInspectionCaseVersions.Add(MapVersions(trashInspection));
+                dbContext.SaveChanges();
+            }  
+        }
+
+        private TrashInspectionCaseVersion MapVersions(TrashInspectionCase trashInspection)
+        {
+            TrashInspectionCaseVersion trashInspectionVersion = new TrashInspectionCaseVersion
+            {
+                Status = trashInspection.Status,
+                TrashInspectionId = trashInspection.TrashInspectionId,
+                SdkCaseId = trashInspection.SdkCaseId,
+                SdkSiteId = trashInspection.SdkSiteId,
+                Version = trashInspection.Version,
+                SegmentId = trashInspection.SegmentId,
+            };
+
+            return trashInspectionVersion;
+        }
     }
 }
