464be21dd1884b689e9f9e604a0c9e9d202cde7c
René Schultz Madsen <rm@microting.dk>
2019-07-02 08:45:53 +0200

    Adding todo for testing.

--- Microting.eFormTrashInspectionBase.Unit.Tests/ProducerUTest.cs
+++ Microting.eFormTrashInspectionBase.Unit.Tests/ProducerUTest.cs
@@ -2,6 +2,6 @@ namespace Microting.eFormTrashInspectionBase.Unit.Tests
 {
     public class ProducerUTest
     {
-        
+        //TODO Write tests
     }
 }
--- Microting.eFormTrashInspectionBase.Unit.Tests/TransporterUTest.cs
+++ Microting.eFormTrashInspectionBase.Unit.Tests/TransporterUTest.cs
@@ -2,6 +2,6 @@ namespace Microting.eFormTrashInspectionBase.Unit.Tests
 {
     public class TransporterUTest
     {
-        
+        //TODO Write tests
     }
 }
