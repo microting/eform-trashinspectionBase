d1faf1c47abbb2bf6ccb8739e21edb4292bf17fc
René Schultz Madsen <rm@microting.dk>
2019-11-19 07:36:44 +0100

    Removing the contstant we don't need here, since it belongs in the angular plugin and not in the base.

--- Microting.eFormTrashInspectionBase/Infrastructure/Const/TrashInspectionClaims.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microting.eFormTrashInspectionBase.Infrastructure.Const
-{
-    public static class TrashInspectionClaims
-    {
-        public const string AccessTrashInspectionPlugin = "trash_inspection_plugin_access";
-        public const string CreateFractions = "fractions_create";
-    }
-}
